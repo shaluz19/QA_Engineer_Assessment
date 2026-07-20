8fe2b73f7791f76e65b4248cad582d87da1b3bab
shaluz19 <21mcaa30@kristujayanti.com>
2026-07-20 22:00:54 +0530

    Fixed proper import path

--- utils/testData.ts
+++ utils/testData.ts
@@ -1,4 +1,4 @@
-export const TEST_EMAIL = "21mcaa30@kristujayanti.com";
+export const TEST_EMAIL = "dummy@example.com";
 export const TEST_PASSWORD = "Test123";
 
 export const TASK_NAME = "Playwright Assignment";
@@ -15,5 +15,5 @@ export const UPDATED_DESCRIPTION_1 =
 `Buy Vegetables and Fruits`;
 
 //API data
-export const API_TOKEN = "ff586b1d7803e0c89a213300dee22783a9090af2";
+export const API_TOKEN = "test_api_token";
 export const API_BASE_URL = "https://api.todoist.com/api/v1";
